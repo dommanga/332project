e873e3d49b7fe05b835631af4fcec75096f960d1
dommanga <leejm21@postech.ac.kr>
2025-12-05 10:20:30 +0900

    vm change

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ RECORDS_PER_WORKER=100000
 DEFAULT_NUM_WORKERS=2
 
 # ALL_WORKERS=("vm01" "vm02" "vm03" "vm04" "vm05" "vm06" "vm07" "vm08" "vm09" "vm10" "vm11" "vm12" "vm13" "vm14" "vm15" "vm16" "vm17" "vm18" "vm19" "vm20")
-ALL_WORKERS=("vm17" "vm18")
+ALL_WORKERS=("vm16" "vm18")
 
 # ============================================
 # 함수 정의
